acc7e808ccd867becb546f39d974e773e1264611
KakisCoding <kauavdsb.jobs@gmail.com>
2025-02-28 14:41:40 -0300

    mexendo com rotas e passando dados do backend pro frontend

--- flaskLearn/routes.py
+++ flaskLearn/routes.py
@@ -1,6 +1,17 @@
 from flaskLearn import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/nova/')
+def novapag():
+    user = "heloísa"
+    namorado = "kauã"
+
+    context = {
+        'user': user,
+        'namorado': namorado
+    }
+    return render_template('oi.html', context=context)
