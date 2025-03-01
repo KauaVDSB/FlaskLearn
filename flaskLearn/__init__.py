ac08856f8b18e10dd2e320ead7b082bacd32c08a
KakisCoding <kauavdsb.jobs@gmail.com>
2025-03-01 18:04:41 -0300

    Banco de dados do Contato modelado e configuracoes de banco de dados realizada.

--- flaskLearn/__init__.py
+++ flaskLearn/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sql_migrate import Migrate
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 
 
 from flaskLearn.routes import homepage
+from flaskLearn.models import Contato
