4500aea301d0d1825276340c707fa684594f319f
KakisCoding <kauavdsb.jobs@gmail.com>
2025-03-05 15:09:16 -0300

    Configurando formulario de cadastro de usuario.

--- flaskLearn/forms.py
+++ flaskLearn/forms.py
@@ -1,12 +1,48 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email # Para validar email, baixar biblioteca email_validator
+from wtforms import StringField, SubmitField, PasswordField
 
-from flaskLearn import db
-from flaskLearn.models import Contato
-
+# Para validar email, baixar biblioteca email_validator
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
+from flaskLearn import db, bcrypt
+from flaskLearn.models import User, Contato
+
+
+# Cadastro de usuario
+class UserForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Sobrenome', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao_senha = PasswordField('Confirmar senha', validators=[DataRequired, EqualTo('senha')])
+    btnSubmit = SubmitField('Cadastrar')
+
+    # def com validate propria para verificar se email e unico. 
+    def validate_email(self, email): # Ao dar submit, ele procura todas as def que comecam com 'validate_'.
+        if User.query.filter(email=email.data).first():
+            return ValidationError('Usuário já cadastrado com este E-mail.')
+
+
+    # Cadastro no banco de dados
+    def save(self):
+        # gera hash para senha criptografada que permite caracteres especiais.
+        senha =  bcrypt.generate_password_hash(self.senha.data.encode('utf-8')) 
+
+        user = User(
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha = senha
+        )
+
+        # salva o usuario na sessão e passa para o banco de dados
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+
+# Contato do usuario
 class ContatoForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
--- flaskLearn/models.py
+++ flaskLearn/models.py
@@ -1,10 +1,16 @@
-from flaskLearn import db
+from flaskLearn import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
 
-class Usuario(db.Model):
+@login_manager.user_loader # Retorna sessao do usuario no controle de login
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=True)
+    sobrenome = db.Column(db.String, nullable=True)
     email = db.Column(db.String, nullable=True)
     senha = db.Column(db.String, nullable=True)
     data_cadastro = db.Column(db.DateTime, default=datetime.now())
